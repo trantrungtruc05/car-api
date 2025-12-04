162db726e8a133df3e13768ee9aa9072897faf08
Truc Tran <tructran@Trucs-MacBook-Pro.local>
2025-12-04 09:53:05 +0700

    Update project

--- app/services/crawler/car_crawler.py
+++ app/services/crawler/car_crawler.py
@@ -45,7 +45,7 @@ def start_crawl(start_page, end_page):
             
             general_info = extract_general_info(content)
             
-            cars_existed = cars_crud.get_car_by_car_id(db=db, car_id=general_info['car_id'])
+            cars_existed = cars_crud.get_car_by_car_id(db=db, car_id=general_info['car_id'] or "")
             if cars_existed:
                 print("------ EXISTED -------")
                 continue
